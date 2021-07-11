fffa954f406feaecd60b2b70a87359c8d1cefe9b
Bartosz Kedziora <bartosz.kedziora89@gmail.com>
2021-07-11 12:04:11 +0200

    Upadte LogOutView

--- src/main/java/com/kodilla/carrental/view/LogOutView.java
+++ src/main/java/com/kodilla/carrental/view/LogOutView.java
@@ -1,5 +1,7 @@
 package com.kodilla.carrental.view;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +9,10 @@ import org.springframework.stereotype.Component;
 public class LogOutView extends VerticalLayout {
 
     public LogOutView() {
-        Button logoutButton = new Button("Log out");
-        add(logoutButton);
+        Button button = new Button("Log out", new Icon(VaadinIcon.EXIT));
+        button.setIconAfterText(true);
+
+        add(button);
+        setHorizontalComponentAlignment(Alignment.CENTER, button);
     }
 }
